daf9d1d8bdc6fb52e690ced1a87eacdb88741f81
rkorsak <rkorsak@gmail.com>
2016-02-24 16:31:14 -0500

    Add license header, fix some styling

--- swiftid/routes/callbacks.js
+++ swiftid/routes/callbacks.js
@@ -31,7 +31,7 @@ module.exports = function (clientId) {
 
     // If the webhookValidationId is "true", then this is just a ping after
     // registering a webhook. There is nothing to do in that case.
-    if(webhookValidationId !== 'true') {
+    if (webhookValidationId !== 'true') {
       var taskStatus = req.body.taskStatus
       var taskReferenceId = req.body.taskReferenceId
 
@@ -45,12 +45,12 @@ module.exports = function (clientId) {
             if (taskStatus === 'APPROVED') {
               photos.addSharedUserId(photo._id, task.requestorId, function (addSharedErr) {})
             }
-          // Notify any listeners that the task has been changed
-          notifier.emit('task-status-changed', {
-            photoId: photo._id,
-            requestorId: task.requestorId,
-            status: taskStatus
-          })
+            // Notify any listeners that the task has been changed
+            notifier.emit('task-status-changed', {
+              photoId: photo._id,
+              requestorId: task.requestorId,
+              status: taskStatus
+            })
           })
         })
       })
